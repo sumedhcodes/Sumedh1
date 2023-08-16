0be95541252346e0936909208747e178df82b593
Imam5414 <61012089+Imam5414@users.noreply.github.com>
2023-08-16 13:18:15 +0530

    Update Kabir1.java
    changes as per requested line no. 4

--- Kabir1.java
+++ Kabir1.java
@@ -1,7 +1,6 @@
 class Kabir1{
 public static void main (String Args[]){
 System.out.println("kabira khada bazar main mange sabki khair, na kahu se dosti na kahu se beir"); 
-  
 System.out.println("kabira khada bazar main mange sabki khair, na kahu se dosti na kahu se beir");
 }
 
